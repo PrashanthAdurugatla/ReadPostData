f3877de8c04955d4ddd9cf284cb3156273552cb8
PrashanthAdurugatla <padurugatla@horizon.csueastbay.edu>
2017-11-06 19:43:02 -0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/readNameAndRespond', function(req, res, next) {
+  var value_name = req.body.name;
+  res.send('Hello' + value_name);
+});
+
+module.exports = router;
